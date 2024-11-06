65c733342c29dbcc8fd60bb9d456adf5e74fd57b
Ogiki Moses Odera <omojick88moses@gmail.com>
2024-11-06 18:09:22 +0300

    more changes

--- lib/widgets/MyApp.dart
+++ lib/widgets/MyApp.dart
@@ -5,6 +5,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
   }
 }
